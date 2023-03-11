5b93576e3b742177878a93701449561723c69e84
Naeem <naeemark@gmail.com>
2023-03-11 21:12:43 +0100

    Initial Commit: #1
    + Project setup
    + Adds .env

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    validationSchema: Joi.object({
+      PORT: Joi.number(),
+    }),
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
