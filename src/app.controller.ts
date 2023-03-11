ad5860b7e0a9acd0d20f2edde1b1124034488dea
Naeem <naeemark@gmail.com>
2023-03-11 21:42:45 +0100

    Adds Swagger #5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,8 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
+  @ApiTags('0 - Health')
+  @ApiOperation({ summary: 'Check Health' })
   getHealth(): {} {
     return this.appService.getHealth();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
+  app.enableVersioning({ type: VersioningType.URI, });
+
+  const config = new DocumentBuilder()
+    .setTitle('Farmlend OMS Service')
+    .setDescription('REST API Specification for Farmlend OMS')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, { swaggerOptions: { defaultModelsExpandDepth: -1 }, });
 
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
