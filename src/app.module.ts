52897848bb0ef2176eb4969bcbeb84ca1110c895
Naeem <naeemark@gmail.com>
2023-03-12 04:27:54 +0100

    CRUD for Product: #7 + Adds controller + Adds service + Adds database config + Add stubs + Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -19,8 +20,9 @@ import { DatabaseModule } from './database/database.module';
       }),
     }),
     DatabaseModule,
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
-@Module({})
-export class DatabaseModule {}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        autoLoadEntities: true,
+        synchronize: true, //should be false at production!
+      }),
+    }),
+  ],
+})
+export class DatabaseModule { }
